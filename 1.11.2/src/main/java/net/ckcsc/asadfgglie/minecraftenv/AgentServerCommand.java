2dae6b30608bf6de5ffb0ad43945385733b837c1
asadfgglie <ac121878@outlook.com>
2025-02-08 22:36:45 +0800

    update 1.11.2 mod

---
+++ 1.11.2/src/main/java/net/ckcsc/asadfgglie/minecraftenv/AgentServerCommand.java
@@ -0,0 +1,87 @@
+package net.ckcsc.asadfgglie.minecraftenv;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AgentServerCommand {
+    private static final Configuration CONFIGURATION = new Configuration();
+    private static SocketIOServer server;
+
+    private static final Logger LOGGER = LogManager.getLogger(AgentServerCommand.class);
+
+    private AgentServerCommand() {}
+
+//    public static void register(CommandDispatcher<CommandSource> dispatcher) {
+//        dispatcher.register(Commands.literal(MinecraftEnv.MOD_ID)
+//            .then(Commands.literal("start")
+//                .executes(source ->
+//                    AgentServerCommand.startSocketServer(source.getSource(), 10000, "localhost"))
+//            .then(Commands.argument("port", IntegerArgumentType.integer(0, 65535))
+//                .executes(source ->
+//                    AgentServerCommand.startSocketServer(source.getSource(), source.getArgument("port", Integer.class), "localhost"))
+//            .then(Commands.argument("host", StringArgumentType.word())
+//                .executes(source ->
+//                    AgentServerCommand.startSocketServer(source.getSource(), source.getArgument("port", Integer.class), source.getArgument("host", String.class)))))));
+//
+//        dispatcher.register(Commands.literal(MinecraftEnv.MOD_ID)
+//            .then(Commands.literal("stop")
+//                .executes(sourceCommandContext -> stopSocketServer())));
+//    }
+
+//    private static int startSocketServer(CommandSource source, int port, String hostName) {
+//        CONFIGURATION.setPort(port);
+//        CONFIGURATION.setHostname(hostName);
+//
+//        if (server == null) {
+//            server = new SocketIOServer(CONFIGURATION);
+//
+//            server.addEventListener("ping", String.class, (client, data, ackSender) -> {
+//                LOGGER.info("Client {} ping: {}, ip: {}", client.getSessionId(), data, client.getRemoteAddress());
+//                source.sendSuccess(new TranslationTextComponent("command.minecraftenv.ping", client.getSessionId(), data, client.getRemoteAddress()), true);
+//                ackSender.sendAckData("pong");
+//            });
+//
+//            server.addEventListener("render", Object.class, (client, data, ackSender) -> {
+////                Minecraft
+//            });
+//
+//            server.addEventListener("step", AgentServerSchema.StepRequest.class, (client, data, ackSender) -> {
+//
+//            });
+//
+//            server.start();
+//            return 0;
+//        }
+//        else {
+//            Configuration cfg = server.getConfiguration();
+//            if (!cfg.getHostname().equals(hostName) || cfg.getPort() != port) {
+//                server.stop();
+//                server = null;
+//                return startSocketServer(source, port, hostName);
+//            }
+//            else {
+//                source.sendSuccess(new TranslationTextComponent("command.minecraftenv.already_start_agent_server", cfg.getHostname(), cfg.getPort()), true);
+//                return 0;
+//            }
+//        }
+//    }
+
+    public static int stopSocketServer() {
+        if (server != null) {
+            server.stop();
+            server = null;
+
+            return 0;
+        }
+        else {
+            LOGGER.warn("Server not started");
+            return -1;
+        }
+    }
+
+    public static SocketIOServer getServer() {
+        return server;
+    }
+}
--- 1.11.2/src/main/java/net/ckcsc/asadfgglie/minecraftenv/MinecraftEnv.java
+++ 1.11.2/src/main/java/net/ckcsc/asadfgglie/minecraftenv/MinecraftEnv.java
@@ -1,20 +1,56 @@
 package net.ckcsc.asadfgglie.minecraftenv;
 
-import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = MinecraftEnv.MODID, version = MinecraftEnv.VERSION)
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
+
+// mark this mod is only work for physical client side
+@Mod(modid = MinecraftEnv.MODID, version = MinecraftEnv.VERSION, name = MinecraftEnv.NAME, clientSideOnly = true)
 public class MinecraftEnv
 {
     public static final String MODID = "minecraftenv";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String NAME = "MinecraftEnv";
+
+    private static final Logger LOGGER = LogManager.getLogger(MinecraftEnv.class);
+
+    private volatile static EntityPlayer agent;
+
+    public MinecraftEnv() {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
     
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    @SubscribeEvent
+    public void onPlayerJoin(PlayerLoggedInEvent event) {
+        LOGGER.info("Player joined in world: {}", event.player.getDisplayName());
+        agent = event.player;
+    }
+
+    @SubscribeEvent
+    public void onPlayerLeave(PlayerLoggedOutEvent event) {
+        LOGGER.info("Player left in world: {}", event.player.getDisplayName());
+        if (AgentServerCommand.getServer() != null) {
+            AgentServerCommand.stopSocketServer();
+        }
+        agent = null;
+    }
+
+//    @SubscribeEvent
+//    public void onCommandRegister(CommandEvent event) {
+//        AgentServerCommand.register(event.getDispatcher());
+//
+//        ConfigCommand.register(event.getDispatcher());
+//    }
+
+    public static EntityPlayer getAgent() {
+        return agent;
     }
 }
